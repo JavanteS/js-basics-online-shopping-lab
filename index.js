4a09bfa728933e14d9d901f7a68d3428908947ed
JavanteS <javantenelson@gmail.com>
2021-01-08 15:24:01 -0600

    commit tree 312fa6c7e2dd777055b42cdc12a06cb943e5cab1

--- index.js
+++ index.js
@@ -5,15 +5,32 @@ function getCart() {
 }
 
 function setCart(c) {
+  
   cart = c;
   return cart;
 }
 
 function addToCart(item) {
+  let hash = {
+    itemName: item,
+    itemPrice: Math.floor(Math.random() * 101) 
+  }
+
+  cart.push(hash) 
+  return `${item} has been added to your cart.`
+  
  // write your code here
 }
 
 function viewCart() {
+
+  if(cart.length === 1){
+
+  }else if(cart.length > 1){
+
+  }else{
+    return 'Your shopping cart is empty.'
+  }
   // write your code here
 }
 
